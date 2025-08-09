2e19e15e7196d9afdd2aa9b79d8e187f9cb358a5
钟爱东 <zhongaidong@wanlianyida.com>
2025-08-09 09:24:38 +0800

    fix: history问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './modules'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.VITE_PUBLIC_PATH || '/'),
   routes,
 })
 
